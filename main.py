1f416a47b4f8c23787d989612c592aadd2fea553
localmanplaysgames <efitton5491@gmail.com>
2025-08-30 12:00:54 +0100

    just cleaning up

--- main.py
+++ main.py
@@ -5,12 +5,9 @@ from google import genai
 from google.genai import types
 
 verbose = '--verbose' in sys.argv
-
 args = [arg for arg in sys.argv[1:] if arg != '--verbose']
-
 if not args:
     sys.exit(1)
-
 user_prompt = ' '.join(args)
 
 load_dotenv()
@@ -20,7 +17,6 @@ client = genai.Client(api_key=api_key)
 messages = [
     types.Content(role='user', parts=[types.Part(text=user_prompt)]),
 ]
-
 response = client.models.generate_content(
     model='gemini-2.0-flash-001',
     contents=messages)
@@ -28,8 +24,13 @@ response = client.models.generate_content(
 prompt_tokens = response.usage_metadata.prompt_token_count
 response_tokens = response.usage_metadata.candidates_token_count
 
-if verbose:
-    print(f'User prompt: {user_prompt}')
-    print(f'Prompt tokens: {prompt_tokens}')
-    print(f'Response tokens: {response_tokens}')
-print(f'Response: {response.text}')
+def main():
+
+    if verbose:
+        print(f'User prompt: {user_prompt}')
+        print(f'Prompt tokens: {prompt_tokens}')
+        print(f'Response tokens: {response_tokens}')
+    print(f'Response: {response.text}')
+
+if __name__ == '__main__':
+    main()
