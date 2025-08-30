d2b58ee64d755d5ac18a8a7a4d316bfc8dd68302
localmanplaysgames <efitton5491@gmail.com>
2025-08-30 04:30:11 +0100

    now takes user input

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+
+if sys.argv[1:] == []: # quit if nothing provided to pass into gemini
+    sys.exit(1)
 
 load_dotenv()
 api_key = os.environ.get('GEMINI_API_KEY')
 client = genai.Client(api_key=api_key)
 
-gemini_response = client.models.generate_content(model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.')
+gemini_response = client.models.generate_content(model='gemini-2.0-flash-001', contents=sys.argv)
 
-print(f'Prompt tokens: {gemini_response.usage_metadata.prompt_token_count}')
-print(f'Response tokens: {gemini_response.usage_metadata.candidates_token_count}')
-print(f'Response: {gemini_response.text}')
+print(f'\nPrompt tokens: {gemini_response.usage_metadata.prompt_token_count}\n')
+print(f'Response tokens: {gemini_response.usage_metadata.candidates_token_count}\n')
+print(f'Response:\n\n{gemini_response.text}')
