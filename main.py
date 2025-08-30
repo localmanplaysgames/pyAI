ccbd83e39a0863b1360ca1e654b7ba68f81076c2
localmanplaysgames <efitton5491@gmail.com>
2025-08-30 04:22:38 +0100

    hardcoded call working

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from pyai!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+load_dotenv()
+api_key = os.environ.get('GEMINI_API_KEY')
+client = genai.Client(api_key=api_key)
 
-if __name__ == "__main__":
-    main()
+gemini_response = client.models.generate_content(model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.')
+
+print(f'Prompt tokens: {gemini_response.usage_metadata.prompt_token_count}')
+print(f'Response tokens: {gemini_response.usage_metadata.candidates_token_count}')
+print(f'Response: {gemini_response.text}')
